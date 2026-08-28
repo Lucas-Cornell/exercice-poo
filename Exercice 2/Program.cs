fe67da5b5edc81faa2a3854e58637b0e594a6ae9
Lucas Cornell <lucascornell24@gmail.com>
2026-08-28 14:20:58 -0400

    first

---
+++ Exercice 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_2
+{
+    class Produit { 
+    
+        public string nom;
+        public double prix;
+        private int nb;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
